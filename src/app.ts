75cdaf154a2f089cd8d2faf7e4b690f00cce331c
Maxim Yushin <maxim.yushin@deliverr.com>
2023-06-21 12:19:17 +0200

    API skeleton

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express, { Response as ExResponse, Request as ExRequest, json, urlencoded } from "express";
+import swaggerUi from "swagger-ui-express";
+import { RegisterRoutes } from "../build/routes";
+
+export const app = express();
+
+app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  return res.send(
+    swaggerUi.generateHTML(await import("../build/swagger.json"))
+  );
+});
+
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
+app.use(json());
+
+RegisterRoutes(app);
