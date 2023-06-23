7792d9357dd242f7de6e8f018b68f9fc9753fa4e
Maxim Yushin <maxim.yushin@deliverr.com>
2023-06-23 21:39:11 +0200

    serverless

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
+import serverless from "serverless-http";
 import express, { Response as ExResponse, Request as ExRequest, json, urlencoded } from "express";
 import swaggerUi from "swagger-ui-express";
-import { RegisterRoutes } from "../build/routes";
+import { RegisterRoutes } from "../.build/routes";
 
 export const app = express();
 
 app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
   return res.send(
-    swaggerUi.generateHTML(await import("../build/swagger.json"))
+    swaggerUi.generateHTML(await import("../.build/swagger.json"))
   );
 });
 
@@ -18,3 +19,5 @@ app.use(
 app.use(json());
 
 RegisterRoutes(app);
+
+export const handler = serverless(app);
